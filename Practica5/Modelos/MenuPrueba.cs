39754cda145e2eb5278f0849f3cb13645a558f02
RaulDuarteBeltran <raul_duarteb@hotmail.com>
2023-12-05 08:34:11 -0600

    Crear menu de pruebas
    Este menú solo servirá para que el programa compile en lo que se crea el menú real.

---
+++ Practica5/Modelos/MenuPrueba.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Practica5.Interfaces;
+
+namespace Practica5.Modelos
+{
+	public class MenuPrueba:IMenu
+	{
+		public MenuPrueba()
+		{
+		}
+
+        public string Titulo { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void AgregarPlatillo(string nombrePlatillo, int precioPlatillo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MostrarMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPlatillo ObtenerPlatillo(int idPlatillo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
