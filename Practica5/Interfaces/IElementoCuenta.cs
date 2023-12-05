7c7360ab4db2827739da904affaf00693f8efada
RaulDuarteBeltran <raul_duarteb@hotmail.com>
2023-12-05 08:14:56 -0600

    Crear interfaz para elementos de una cuenta
    - El platillo no debe ser modificable luego de la creación de un elemento.
    - El costo total debería ser una propiedad calculada, así que lo dejamos de solo lectura.

---
+++ Practica5/Interfaces/IElementoCuenta.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Practica5.Interfaces
+{
+	public interface IElementoCuenta
+	{
+		IPlatillo Platillo { get; }
+		int Cantidad { get; set; }
+		int CostoTotal { get; }
+	}
+}
+
