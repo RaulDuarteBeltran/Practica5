8323ff557d48d78200794662a184a687a71296cb
RaulDuarteBeltran <raul_duarteb@hotmail.com>
2023-12-05 08:17:27 -0600

    Crear interfaz para cuentas
    El costo total es calculado, así que lo dejamos de solo lectura.

---
+++ Practica5/Interfaces/ICuenta.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Practica5.Interfaces
+{
+	public interface ICuenta
+	{
+		int CostoTotal { get; }
+		void AgregarElemento(IPlatillo platillo, int cantidad);
+		void MostrarCuenta();
+	}
+}
+
