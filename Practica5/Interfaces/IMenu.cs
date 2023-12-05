99a52e0d30b550b49b22525c6d75bcc5def010da
RaulDuarteBeltran <raul_duarteb@hotmail.com>
2023-12-05 08:12:08 -0600

    Agregar interfaz para Menús

---
+++ Practica5/Interfaces/IMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Practica5.Interfaces
+{
+	public interface IMenu
+	{
+		string Titulo { get; set; }
+		void MostrarMenu();
+		void AgregarPlatillo(string nombrePlatillo, int precioPlatillo);
+		IPlatillo ObtenerPlatillo(int idPlatillo);
+
+	}
+}
+
