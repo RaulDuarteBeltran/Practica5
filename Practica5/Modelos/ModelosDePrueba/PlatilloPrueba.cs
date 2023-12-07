7552bd5fd3e0d08a467a834cf25816b3b5eddff7
RaulDuarteBeltran <raul_duarteb@hotmail.com>
2023-12-07 08:00:30 -0600

    Crear Platillo de prueba

---
+++ Practica5/Modelos/ModelosDePrueba/PlatilloPrueba.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Practica5.Interfaces;
+
+namespace Practica5.Modelos.ModelosDePrueba
+{
+	public class PlatilloPrueba : IPlatillo
+	{
+		public PlatilloPrueba()
+		{
+		}
+
+        public string Nombre { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Precio { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Id => throw new NotImplementedException();
+    }
+}
+
