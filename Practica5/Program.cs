21353d2436fa2da6b6710a7aee360c62cd8b8eeb
RaulDuarteBeltran <raul_duarteb@hotmail.com>
2023-12-07 08:15:32 -0600

    Crear menu principal

--- Practica5/Program.cs
+++ Practica5/Program.cs
@@ -1,9 +1,63 @@
-﻿namespace Practica5;
+﻿using Practica5.Interfaces;
+using Practica5.Modelos;
+
+namespace Practica5;
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        while(true)
+        {
+            MostrarMenuPrincipal();
+            Console.WriteLine("Seleccione opción:");
+            string opcionSeleccionada = Console.ReadLine();
+
+            switch(opcionSeleccionada)
+            {
+                case "1":
+                    ManejarOpcionAdministrarMenus();
+                    break;
+                    
+            }
+        }
+    }
+
+    static void MostrarMenuPrincipal()
+    {
+        Console.WriteLine("1) Administrar Menús");
+        Console.WriteLine("2) Administrar Cuentas");
+        Console.WriteLine("3) Salir");
+    }
+
+    static void ManejarOpcionAdministrarMenus()
+    {
+        while (true)
+        {
+            MostrarMenuAdministracionMenus();
+            Console.WriteLine("Seleccione opción:");
+            string opcionSeleccionada = Console.ReadLine();
+
+            switch (opcionSeleccionada)
+            {
+                case "1":
+                    ManejarOpcionAgregarMenuNuevo();
+                    break;
+            }
+        }
+    }
+
+    static void MostrarMenuAdministracionMenus()
+    {
+        Console.WriteLine("Agregar Menu nuevo");
+    }
+
+    static void ManejarOpcionAgregarMenuNuevo()
+    {
+        Console.WriteLine("Dime el titulo del nuevo menu:");
+        string titulo = Console.ReadLine();
+        IRestaurante restaurante = new RestaurantePrueba();
+        restaurante.CrearMenu(titulo);
     }
 }
 
