642ab231d172d8a9b844bbe7f1d8894865169142
RaulDuarteBeltran <raul_duarteb@hotmail.com>
2023-12-07 08:02:39 -0600

    Crear clase de prueba para restaurante

---
+++ Practica5/Modelos/ModelosDePrueba/RestaurantePrueba.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Practica5.Interfaces;
+
+namespace Practica5.Modelos.ModelosDePrueba
+{
+	public class RestaurantePrueba : IRestaurante
+	{
+		public RestaurantePrueba()
+		{
+		}
+
+        public void AgregarElementosAMenu(IMenu menu)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMenu CrearMenu(string tituloMenu)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MostrarMenus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMenu ObtenerMenu(int indiceMenu)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
